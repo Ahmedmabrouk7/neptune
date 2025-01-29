48e65877c76f8acfa0f805ef5f009221a7e51f2d
Ahmedmabrouk7 <ahmedmabrouk7@gmail.com>
2025-01-30 00:39:56 +0200

    End part two

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:neptune/boxes.dart';
+import 'package:neptune/person.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -7,14 +9,88 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+//====================================================================
 class _HomePageState extends State<HomePage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController subjectController = TextEditingController();
+  TextEditingController dateController = TextEditingController();
+  //====================================================================
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 29, 29, 29),
+      backgroundColor: const Color.fromARGB(255, 2, 19, 41),
       appBar: AppBar(
         title: Text("Neptune notes"),
         backgroundColor: Colors.orange,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 10.0),
+          Image.network(
+              'https://images-platform.99static.com/34OixtNawXfG4ugxds2Qqnl2aQo=/332x30:1680x1378/500x500/top/smart/99designs-contests-attachments/74/74261/attachment_74261695',
+              height: 150),
+          Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    // text filed Title
+                    TextField(
+                      controller: titleController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Title',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    // text filed Subject
+                    TextField(
+                      controller: subjectController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Subject',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    // text filed date
+                    TextField(
+                      controller: dateController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Date',
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    // Add Button
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          boxPersons.put(
+                            'key_${titleController.text}',
+                            Person(
+                              title: titleController.text,
+                              subject: subjectController.text,
+                              date: DateTime.parse(dateController.text),
+                            ),
+                          );
+                        });
+                      },
+                      child: const Text('Add note'),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
